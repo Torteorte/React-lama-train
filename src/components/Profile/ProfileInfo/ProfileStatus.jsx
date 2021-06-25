7b97a1f5e76cc724eb1679ab0ba1c59ad7776b81
Torteorte <lipstort@gmail.com>
2021-06-25 18:46:24 +0300

    update component status

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -28,6 +28,14 @@ class ProfileStatus extends React.Component {
       })
    }
 
+   componentDidUpdate(prevProps, prevStatus) {
+      if (prevProps.status !== this.props.status) {
+         this.setState({
+            status: this.props.status
+         })
+      }
+   }
+
    render() {
       return (
          <div className={styles.status}>
